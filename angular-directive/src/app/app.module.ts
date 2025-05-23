fc92f3c96dc9cd38679033d5050005e5ad8afc72
Deepasri <mskdeepasri@gmail.com>
2025-05-23 22:32:05 +0530

    Custom directive

--- angular-directive/src/app/app.module.ts
+++ angular-directive/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DemoComponent } from './demo/demo.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DemoComponent
+    DemoComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
--- angular-directive/src/app/demo/demo.component.ts
+++ angular-directive/src/app/demo/demo.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { HighlightDirective } from '../highlight.directive';
 @Component({
   selector: 'app-demo',
   templateUrl: './demo.component.html',
